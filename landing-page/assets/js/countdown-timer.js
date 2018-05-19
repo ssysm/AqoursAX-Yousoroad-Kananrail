af9897ebcbe9304c657dece74ab67e88d8b61422
ssysm <me@theeditorstudio.com>
2018-05-18 21:20:57 -0400

    quick push

--- landing-page/assets/js/countdown-timer.js
+++ landing-page/assets/js/countdown-timer.js
@@ -16,7 +16,6 @@ var update = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
